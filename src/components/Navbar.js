a0d50dcac642fe08e32ba9cb155530ea4f308413
Saurabhm000 <mauryasaurabh9698@gmail.com>
2025-12-09 14:18:47 +0530

    day3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,8 +5,8 @@ import PropTypes from 'prop-types'
 // {Below Here We are Accepting Props in Navbar Function}
 function Navbar(props) {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div className="container-fluid ">
                 <a className="navbar-brand" href="/">{props.title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
--- src/components/Textform.js
+++ src/components/Textform.js
@@ -1,24 +1,37 @@
 import React,{useState} from 'react'
 
 function Textform(props) {
-    const [text, setText] = useState("Enter Text Here");
+    const [text, setText] = useState("");
 
     const addTextValue = (event)=>{
         setText(event.target.value);
+        
     }
 
     const buttonUpClick = ()=>{
        let newText = text.toUpperCase();
        setText(newText);
     }
+    const buttonLowClick = ()=>{
+       let newText = text.toLowerCase();
+       setText(newText);
+    }
+    
     return (
-        <div>
+        <>
+        <div className='container my-4'>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className='form-control' id='mybox' value={text} onChange={addTextValue} rows="8"></textarea>
+                <textarea className='form-control' id='mybox' value={text} onChange={addTextValue}  rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={buttonUpClick}>Conver to UpperCase</button>
+            <button className="btn btn-primary mx-2" onClick={buttonUpClick}>Conver to UpperCase</button>
+            <button className="btn btn-primary" onClick={buttonLowClick}>Conver to LowerCase</button>
         </div>
+        <div className='container'>
+            <h2>Your Paragraph have {text.length} Characters including spaces and {text.split(' ').length}  words</h2>
+            <p>{text}</p>
+        </div>
+        </>
     )
 }
 
